945f82f8c761d994ce22917d3f9cd08252a78f3b
thatsnotamuffin <bretlrickerson@gmail.com>
2022-11-18 23:07:36 -0600

    Reverted gathering a list of files - github actions provides file names in a different view

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
 if [[ -n $TARGET_DIRECTORY ]] && [[ -n $CUSTOM_CONFIG ]]; then
-    yaml_list=( $(find $TARGET_DIRECTORY \( -name "*.yaml" -o -name "*yml" \)) )
-    
-    for file in ${yaml_list[@]}; do
-        yamllint -c $CUSTOM_CONFIG $file
-    done
-
-elif [[ -n $TARGET_DIRECTORY ]] && [[ -z $CUSTOM_CONFIG ]]; then
-    yaml_list=( $(find $TARGET_DIRECTORY \( -name "*.yaml" -o -name "*yml" \)) )
-    
-    for file in ${yaml_list[@]}; do
-        yamllint $TARGET_DIRECTORY
-    done
-
+    yamllint -c $CUSTOM_CONFIG $TARGET_DIRECTORY
 elif [[ -z $TARGET_DIRECTORY ]] && [[ -n $CUSTOM_CONFIG ]]; then
-    yaml_list=( $(find . \( -name "*.yaml" -o -name "*yml" \)) )
-    
-    for file in ${yaml_list[@]}; do
-        yamllint -c $CUSTOM_CONFIG $file
-    done
-
+    yamllint -c $CUSTOM_CONFIG .
+elif [[ -n $TARGET_DIRECTORY ]] && [[ -z $CUSTOM_CONFIG ]]; then
+    yamllint $TARGET_DIRECTORY
 else
-    yaml_list=( $(find . \( -name "*.yaml" -o -name "*yml" \)) )
-    
-    for file in ${yaml_list[@]}; do
-        yamllint .
-    done
+    yamllint .
 fi
